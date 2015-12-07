1455a6a01fd519a30f2e1283f62fb311719c2b82
Daniel Santana <daniel@everymundo.com>
2015-12-06 23:29:36 -0500

    messages added to setup.sh script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ BASE_DIR=$(dirname $PROJECT_GIT_DIR)
 cd $BASE_DIR
 
 # getting spark from git repo
+echo "##### Cloning SPARK"
 SPARK_GIT_DIR=$BASE_DIR/spark.git
 time git clone git://github.com/apache/spark.git $SPARK_GIT_DIR
 echo
@@ -15,10 +16,10 @@ time build/mvn -DskipTests clean package
 echo
 
 #getting mongodb-hadoop from
+echo "##### Cloning MongoDB Hadoop Connector Project"
 HADOOP_GIT_DIR=$BASE_DIR/mongodb-hadoop.git
 git clone git@github.com:mongodb/mongo-hadoop.git $HADOOP_GIT_DIR
-cd $HADOOP_GIT_DIR
-./gradlew jar
+echo
 
 echo "##### Cloning MongoDB Hadoop Connector Project"
 patch $HADOOP_GIT_DIR/core/src/main/java/com/mongodb/hadoop/util/MongoConfigUtil.java $PROJECT_GIT_DIR/com_mongodb_hadoop_util_MongoConfigUtil.java.patch
@@ -30,22 +31,32 @@ time ./gradlew jar
 echo
 
 # Dependency jars
+echo "##### Copying the requred JAR files to the jars directory"
 cd $PROJECT_GIT_DIR
 mkdir jars
 cp -a $(find $HADOOP_GIT_DIR -name mongo-hadoop-core\*.jar) ./jars/
 cp -a $(find $HADOOP_GIT_DIR -name mongo-hadoop-spark\*.jar) ./jars/
-curl https://oss.sonatype.org/content/repositories/releases/org/mongodb/mongodb-driver/3.1.1/mongodb-driver-3.1.1.jar > jars/mongodb-driver-3.1.1.jar
+time curl https://oss.sonatype.org/content/repositories/releases/org/mongodb/mongodb-driver/3.1.1/mongodb-driver-3.1.1.jar > jars/mongodb-driver-3.1.1.jar
+echo
 
+echo "##### Downloading the sample data"
 TEMP_DIR=./tmp
 TEMP_FILE=mstf.csv
 mkdir $TEMP_DIR
-
 curl http://www.barchartmarketdata.com/data-samples/$TEMP_FILE > $TEMP_DIR/$TEMP_FILE
+echo
 
 DB_NAME=marketdata
 COLL_NAME=minibars
 
+echo "##### Importing the sample data to mongodb://localhost/${DB_NAME}.${COLL_NAME}"
 mongo $DB_NAME --eval "db.${COLL_NAME}.drop()"
-mongoimport $TEMP_DIR/$TEMP_FILE --type csv --headerline -d $DB_NAME -c $COLL_NAME
+time mongoimport $TEMP_DIR/$TEMP_FILE --type csv --headerline -d $DB_NAME -c $COLL_NAME
+echo
 
-sudo easy_install pytz
+
+echo "##### Installing some python Dependency"
+time sudo easy_install pytz
+echo
+
+echo "##### Done!"
